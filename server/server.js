ec1748ef8e847fb598cda1ef120a79ac3c418ac2
sribhoomiorganics01-lab <sribhoomiorganics01@gmail.com>
2026-05-02 21:07:56 +0530

    fix double api issue

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+const path = require('path');
 
 dotenv.config();
 
@@ -30,7 +31,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(morgan('dev'));
 
 app.use('/api/auth', authRoutes);
