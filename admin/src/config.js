b72885081026548f4efd0ce43808fd845bf9d727
sribhoomiorganics01-lab <sribhoomiorganics01@gmail.com>
2026-05-02 21:01:19 +0530

    fix double api issue

--- admin/src/config.js
+++ admin/src/config.js
@@ -1,5 +1,5 @@
 const BASE_URL = process.env.REACT_APP_BASE_URL ||'https://sribhoomiorganics.onrender.com/api';
 
-const API_URL = `${BASE_URL}/api`;
+const API_URL = BASE_URL;
 
 export { API_URL, BASE_URL };
