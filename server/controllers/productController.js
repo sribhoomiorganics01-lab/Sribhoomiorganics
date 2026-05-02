2fe95114938cc5304de7a5796b60b219a82d96e3
sribhoomiorganics01-lab <sribhoomiorganics01@gmail.com>
2026-05-02 23:02:48 +0530

    fix image path final

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -252,7 +252,7 @@ exports.createProduct = async (req, res) => {
       featured,
       bestSeller,
       organic,
-      image: req.file ? req.file.filename : ''
+      image: req.file ? `/uploads/${req.file.filename}` : ''
     });
 
     await product.save();
@@ -281,7 +281,7 @@ exports.updateProduct = async (req, res) => {
 
     // 🔥 IMAGE FIX
     if (req.file) {
-      product.image = req.file.filename;
+      product.image = `/uploads/${req.file.filename}`;
     }
 
     await product.save();
