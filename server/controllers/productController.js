123effd7f8e26f97765ab7f96d9f92b7348120ed
sribhoomiorganics01-lab <sribhoomiorganics01@gmail.com>
2026-05-03 13:08:11 +0530

    fix cloudinary config final

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -2,6 +2,12 @@ const Product = require('../models/Product');
 const Category = require('../models/Category');
 const cloudinary = require("cloudinary").v2;
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 exports.getProducts = async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
