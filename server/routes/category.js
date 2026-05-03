9fa65a21b1af2da333a50ff4590a1a89bca0a179
sribhoomiorganics01-lab <sribhoomiorganics01@gmail.com>
2026-05-03 11:33:22 +0530

    remove disk storage multer completely

--- server/routes/category.js
+++ server/routes/category.js
@@ -4,17 +4,7 @@ const Category = require('../models/Category');
 const { protect, admin } = require('../middleware/auth');
 const Product = require('../models/Product');
 const multer = require('multer');
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/');
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname);
-  }
-});
-
-const upload = multer({ storage });
+const upload = require('../middleware/upload');
 
 router.get('/', async (req, res) => {
   try {
