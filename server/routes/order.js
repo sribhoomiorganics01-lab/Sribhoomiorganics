51114b1c787c5adc282fffe676512d798a76d60e
sribhoomiorganics01-lab <sribhoomiorganics01@gmail.com>
2026-05-04 23:41:00 +0530

    Fix failOrder import error

--- server/routes/order.js
+++ server/routes/order.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createOrder, verifyPayment, deleteOrder, getMyOrders, getOrderById, cancelOrder } = require('../controllers/orderController');
+const { createOrder, verifyPayment, deleteOrder, getMyOrders, getOrderById, cancelOrder,failOrder } = require('../controllers/orderController');
 const { protect } = require('../middleware/auth');
 
 router.post('/', protect, createOrder);
